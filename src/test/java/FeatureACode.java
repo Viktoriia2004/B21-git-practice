e24c3c39ea811513d73d18ed813e3a5eb9bba13c
Viktoriia2004 <vshishulina@mail.ru>
2021-01-27 15:30:39 -0500

    more work on featureA on new branch

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -2,5 +2,6 @@ public class FeatureACode {
     public static void main(String[] args) {
 
         System.out.printf("Awesome work for A started");
+        System.out.printf("B21 is learning more git with review");
     }
 }
