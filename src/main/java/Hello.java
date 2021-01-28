7aa6e4f20e26ab84c47b0442fcdc6020391a119e
Viktoriia2004 <vshishulina@mail.ru>
2021-01-28 00:04:08 -0500

    master branch wathing on the living room for 10 min

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,4 +4,6 @@ public class Hello {
         System.out.println("Hello World B21");
         System.out.printf("B21 future SDET learning GIT");
     }
+
+    //oh, I like this movie, let's continue
 }
